e314334aaf417e81925c28ac02fb138f897810ba
William L <41084946+helveticae@users.noreply.github.com>
2023-05-07 16:55:30 +0200

    titles_by_author view

---
+++ Scripts/utils.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+# Validate ISBN
+# Source https://rosettacode.org/wiki/ISBN13_check_digit
+
+# Validate the check digit of an ISBN-13 code:
+
+#  Multiply every other digit by  3.
+#  Add these numbers and the other digits.
+#  Take the remainder of this number after division by  10.
+#  If it is  0,   the ISBN-13 check digit is correct.
+
+def validate_isbn(n):
+    n = n.replace('-','').replace(' ', '')
+    if len(n) != 13:
+        return False
+    product = (sum(int(ch) for ch in n[::2]) 
+               + sum(int(ch) * 3 for ch in n[1::2]))
+    return product % 10 == 0
+
+# Vy: ”TitlarPerFörfattare”
+def titles_by_author():
+    conn = sqlite3.connect('amazun.db')
+    c = conn.cursor()
+
+    # Create the view
+    c.execute('''CREATE VIEW TitlarPerFörfattare AS
+                    SELECT Author.Name || ' ' || Author.Surname AS Namn,
+                           strftime('%Y', 'now') - strftime('%Y', Author.Birthdate) AS Ålder,
+                           COUNT(DISTINCT Book.Title) AS Titlar,
+                           SUM(Book.Price * Inventory.Stock) AS Lagervärde
+                    FROM Author
+                    JOIN Book ON Author.ID = Book.AuthID
+                    JOIN Inventory ON Book.ISBN13 = Inventory.ISBN13
+                    GROUP BY Author.Name, Author.Surname
+                    ORDER BY Namn''')
+
+    conn.commit()
+    conn.close()
+
+
+# Drop table OR view
+# https://www.sqlitetutorial.net/sqlite-create-view/
+
+def drop_table(name, file_name='amazun.db'):
+    conn = sqlite3.connect(file_name)
+    c = conn.cursor()
+
+    try:
+        c.execute(f"DROP TABLE {name}")
+    except sqlite3.OperationalError:
+        try:
+            c.execute(f"DROP VIEW {name}")
+        except sqlite3.OperationalError:
+            print(f"No table or view named {name} found in database.")
+
+    conn.commit()
+    conn.close()
+
+drop_table('TitlarPerFörfattare')
