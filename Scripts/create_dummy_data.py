45b52f1ab2b4d0f77333f715ec5665ba7d6c18f3
William L <41084946+helveticae@users.noreply.github.com>
2023-05-07 13:43:39 +0200

    increase dummy data books

--- Scripts/create_dummy_data.py
+++ Scripts/create_dummy_data.py
@@ -16,14 +16,19 @@ books = [
     ('9780007117116', 'The Lord of the Rings', 'English', 29.99, datetime(1954, 7, 29), 1),
     ('9780439554930', 'Harry Potter and the Philosopher\'s Stone', 'English', 9.99, datetime(1997, 6, 26), 2),
     ('9780553801477', 'Foundation', 'English', 19.99, datetime(1951, 5, 1), 3),
-    ('9780553588488', 'Ender\'s Game', 'English', 14.99, datetime(1985, 1, 1), 4)
+    ('9780553588488', 'Ender\'s Game', 'English', 14.99, datetime(1985, 1, 1), 4),
+    ('9781400031702', 'The Picture of Dorian Gray', 'English', 10.99, datetime(1890, 7, 1), 5),
+    ('9780141187761', 'Nineteen Eighty-Four', 'English', 12.99, datetime(1949, 6, 8), 6),
+    ('9780316346627', 'A Game of Thrones', 'English', 16.99, datetime(1996, 8, 1), 7),
+    ('9780061124952', 'American Gods', 'English', 15.99, datetime(2001, 6, 19), 8),
+    ('9780679745587', 'One Hundred Years of Solitude', 'Spanish', 18.99, datetime(1967, 5, 30), 9),
+    ('9780553382563', 'The Hitchhiker\'s Guide to the Galaxy', 'English', 12.99, datetime(1979, 10, 12), 10)
 ]
 
 stores = [
     ('123 Main St.', 'New York'),
     ('456 Elm St.', 'Los Angeles'),
     ('789 Oak St.', 'Chicago'),
-    ('321 Pine St.', 'Houston')
 ]
 
 # Define some functions to generate random data
