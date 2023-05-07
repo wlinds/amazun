3cb3c41c4ce08765958a5684011c00612b05f1d9
William L <41084946+helveticae@users.noreply.github.com>
2023-05-07 13:55:05 +0200

    titles by author function

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 # Validate ISBN
 # Source https://rosettacode.org/wiki/ISBN13_check_digit
 
@@ -15,3 +17,28 @@ def validate_isbn(n):
     product = (sum(int(ch) for ch in n[::2]) 
                + sum(int(ch) * 3 for ch in n[1::2]))
     return product % 10 == 0
+
+
+
+
+# Vy: ”TitlarPerFörfattare”
+def titles_by_author():
+    conn = sqlite3.connect('amazun.db')
+    c = conn.cursor()
+
+    # Create the view
+    c.execute('''CREATE VIEW TitlarPerFörfattare AS
+                    SELECT Author.Name || ' ' || Author.Surname AS Namn,
+                           strftime('%Y', 'now') - strftime('%Y', Author.Birthdate) AS Ålder,
+                           COUNT(DISTINCT Book.Title) AS Titlar,
+                           SUM(Book.Price * Inventory.Stock) AS Lagervärde
+                    FROM Author
+                    JOIN Book ON Author.ID = Book.AuthID
+                    JOIN Inventory ON Book.ISBN13 = Inventory.ISBN13
+                    GROUP BY Author.Name, Author.Surname
+                    ORDER BY Namn''')
+
+    conn.commit()
+    conn.close()
+
+titles_by_author()
