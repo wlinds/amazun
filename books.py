b5e135f875edb8dd1e4f5646b24a2b6c02b83dca
William L <41084946+helveticae@users.noreply.github.com>
2023-05-08 17:38:15 +0200

    improved burn_book(), create add_to_inventory()

--- books.py
+++ books.py
@@ -7,6 +7,10 @@ from sqlalchemy.orm.exc import NoResultFound
 # NoResultFound is currently used only in burn_book() to check if ISBN exist in db
 
 def search_books(search_term):
+    """
+    Search for books currently in store
+    """
+
     session = Session()
     books = (
         session.query(Book, Inventory, Store)
@@ -24,14 +28,18 @@ def search_books(search_term):
     return results
 
 def add_book(title, language, price, release_date, author_id, isbn, validate=False, verbose=False):
+    """
+    Add a book to Book table.
+    """
+    
+    #TODO: checksum is broken, look into
     isbn = str(isbn) 
-
     if validate:
         checksum = validate_isbn(isbn)
-        if checksum == 0:
-            print('ISBN PASS!')
-        else:
+        if checksum != 0:
             raise ValueError('ISBN did not pass validation.')
+        else:
+            print('ISBN PASS!')
 
     session = Session() #TODO Move this to outside function for performance improvements
 
@@ -56,29 +64,56 @@ def add_book(title, language, price, release_date, author_id, isbn, validate=Fal
     if verbose:
         print(f'Added book with ISBN: {isbn} to table.')
 
+def add_to_inventory(isbn, store_id, stock):
+    session = Session()
+    inventory = session.query(Inventory).filter_by(ISBN13=isbn, StoreID=store_id).first()
+    if inventory:
+        inventory.Stock += stock
+    else:
+        inventory = Inventory(ISBN13=isbn, StoreID=store_id, Stock=stock)
+        session.add(inventory)
+    session.commit()
+
+
+
+
+
+
 def burn_book(isbn, verbose=False):
     session = Session()
-    book_to_remove = None
 
     try:
         book_to_remove = session.query(Book).filter_by(ISBN13=isbn).one()
-
     except NoResultFound:
         session.close()
         print(f'Book with ISBN {isbn} does not exist in the database.')
+        return
 
-    if book_to_remove:
-        session.delete(book_to_remove)
-        session.commit()
-        session.close()
-        if verbose:
-            print(f'Book with ISBN {isbn} has been removed from the database.')
+    try:
+        inventory_to_remove = session.query(Inventory).filter_by(ISBN13=isbn).all()
+    except NoResultFound:
+        inventory_to_remove = None
+
+    if inventory_to_remove:
+        for inventory in inventory_to_remove:
+            session.delete(inventory)
+
+    session.delete(book_to_remove)
+    session.commit()
+    session.close()
+
+    if verbose:
+        print(f'Book with ISBN {isbn} and its associated inventory have been removed from the database.')
+
 
 if __name__ == '__main__':
 
     # These functions are still a bit wonky TODO
-    burn_book("9780123456782", verbose=True)
-    add_book('Book7', "English", "12.99", "2023-05-05", 1337, "9780123456782", validate=True, verbose=True)
+    
+    #burn_book("978012345", verbose=True)
+    #add_book('Book7', "English", "12.99", "2023-05-05", 1337, "978012345", validate=True, verbose=True)
+
+    add_to_inventory("9780553801477", 1, 99)
 
     results = search_books('Hitchhiker')
     for title, stores in results.items():
