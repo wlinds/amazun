5e9d6f4f9c30d76bd1a59523af4808d7c3f4596e
William L <41084946+helveticae@users.noreply.github.com>
2023-05-09 11:57:39 +0200

    get_dummy_cst, TODO: var changes (snake_case)

--- customer.py
+++ customer.py
@@ -38,12 +38,12 @@ def purchase_book(isbn, store_id, customer_id, quantity):
 
     # Get the name of the book, store and customer (TODO: probably an easier way to do this)
     book_name = session.query(Book.Title).filter_by(ISBN13=isbn).scalar()
-    store_name = session.query(Store.Store_Name).filter_by(ID=store_id).scalar()
-    customer_name = session.query(Customer.Name).filter_by(ID=customer_id).scalar()
+    store_name = session.query(Store.store_name).filter_by(ID=store_id).scalar()
+    customer_name = session.query(Customer.name).filter_by(ID=customer_id).scalar()
 
     # Total books owned and total spending
     total_books_owned = session.query(func.sum(CustomerBooks.copies_owned)).filter_by(customer_id=customer_id).scalar()
-    total_spending = session.query(func.sum(Transaction.Total_Cost)).filter_by(CustomerID=customer_id).scalar()
+    total_spending = session.query(func.sum(Transaction.total_cost)).filter_by(CustomerID=customer_id).scalar()
 
     unique_books_count = session.query(func.count(distinct(CustomerBooks.book_id))).filter_by(customer_id=customer_id).scalar()
 
@@ -55,6 +55,24 @@ def purchase_book(isbn, store_id, customer_id, quantity):
     if remaining_stock < 10:
         print(f"Heads up! There are only {remaining_stock} copies of '{book_name}' (ISBN: {isbn}) left in stock at '{store_name}'. Order new?")
 
+def get_dummy_cst():
+
+    # Dummy data for Customer table
+    customer_data = [
+        ('John', 'Doe', 'johndoe@example.com', '123 Main St', 'New York', 'NY', '10001'),
+        ('Jane', 'Smith', 'janesmith@example.com', '456 Elm St', 'Los Angeles', 'CA', '90001'),
+        ('Bob', 'Johnson', 'bobjohnson@example.com', '789 Oak St', 'Chicago', 'IL', '60601'),
+        ('Alice', 'Lee', 'alicelee@example.com', '321 Maple St', 'Houston', 'TX', '77001'),
+        ('David', 'Brown', 'davidbrown@example.com', '654 Pine St', 'Philadelphia', 'PA', '19101')
+    ]
+
+    with Session() as session:
+        for name, surname, address, city, state, zipcode, email in customer_data:
+            new_customer = Customer(name=name, surname=surname, address=address, city=city, state=state, zipcode=zipcode, email=email)
+            session.add(new_customer)
+            session.commit()
+
 if __name__ == '__main__':
-    Base.metadata.create_all(bind=engine) # Currently only used to add table CustomerBooks(Base)
-    purchase_book("9780007117116", 1, 1, 2)
+    pass
+    #Base.metadata.create_all(bind=engine) # Currently only used to add table CustomerBooks(Base)
+    #purchase_book("9780007117116", 1, 1, 2)
