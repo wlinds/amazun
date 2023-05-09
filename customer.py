3a88607f2c9bcd7aabb932ad35522cd969e0adcf
William L <41084946+helveticae@users.noreply.github.com>
2023-05-09 09:16:32 +0200

    update customer's book list, adjust indentation

--- customer.py
+++ customer.py
@@ -1,6 +1,7 @@
 # For customers
 from models import * 
 
+
 # Hello and wälcome to the bookstöre amazun pls purchase a böwk
 
 def purchase_book(isbn, store_id, customer_id, quantity):
@@ -24,20 +25,36 @@ def purchase_book(isbn, store_id, customer_id, quantity):
         transaction = Transaction(ISBN13=isbn, StoreID=store_id, CustomerID=customer_id, Quantity=quantity, Total_Cost=total_cost)
         session.add(transaction)
 
+        # Update the customer's list of books
+        customer_book = session.query(CustomerBooks).filter_by(customer_id=customer_id, book_id=isbn).first()
+        if customer_book:
+            customer_book.copies_owned += quantity
+        else:
+            customer_book = CustomerBooks(customer_id=customer_id, book_id=isbn, copies_owned=quantity)
+            session.add(customer_book)
+
         # Commit the changes to the database
         session.commit()
 
-        # Get the name of the book, store and customer (TODO: probably an easier way to do this)
-        book_name = session.query(Book.Title).filter_by(ISBN13=isbn).scalar()
-        store_name = session.query(Store.Store_Name).filter_by(ID=store_id).scalar()
-        customer_name = session.query(Customer.Name).filter_by(ID=customer_id).scalar()
+    # Get the name of the book, store and customer (TODO: probably an easier way to do this)
+    book_name = session.query(Book.Title).filter_by(ISBN13=isbn).scalar()
+    store_name = session.query(Store.Store_Name).filter_by(ID=store_id).scalar()
+    customer_name = session.query(Customer.Name).filter_by(ID=customer_id).scalar()
 
-        print(f"{customer_name} purchased {quantity} copies of '{book_name}' (ISBN: {isbn}) from '{store_name}' for a total cost of {total_cost}.")
+    # Total books owned and total spending
+    total_books_owned = session.query(func.sum(CustomerBooks.copies_owned)).filter_by(customer_id=customer_id).scalar()
+    total_spending = session.query(func.sum(Transaction.Total_Cost)).filter_by(CustomerID=customer_id).scalar()
 
-        remaining_stock = session.query(Inventory.Stock).filter_by(ISBN13=isbn, StoreID=store_id).scalar()
+    unique_books_count = session.query(func.count(distinct(CustomerBooks.book_id))).filter_by(customer_id=customer_id).scalar()
 
-        print(f"There are {remaining_stock} copies of '{book_name}' (ISBN: {isbn}) left in stock at '{store_name}'.")
+    print(f"{customer_name} purchased {quantity} copies of '{book_name}' (ISBN: {isbn}) from '{store_name}' for a total cost of {total_cost}.")
+    print(f"{customer_name} now owns {total_books_owned} books ({unique_books_count} unique) and has a total spending of {total_spending}.")
 
+    remaining_stock = session.query(Inventory.Stock).filter_by(ISBN13=isbn, StoreID=store_id).scalar()
+
+    if remaining_stock < 10:
+        print(f"Heads up! There are only {remaining_stock} copies of '{book_name}' (ISBN: {isbn}) left in stock at '{store_name}'. Order new?")
 
 if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine) # Currently only used to add table CustomerBooks(Base)
     purchase_book("9780007117116", 1, 1, 2)
