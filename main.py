56af25beacdcd89e7c97ff4de92468c02f2e6026
William L <41084946+helveticae@users.noreply.github.com>
2023-05-09 14:45:35 +0200

    continue testing search, purchase and move books

--- main.py
+++ main.py
@@ -5,28 +5,39 @@ from Scripts.utils import titles_by_author
 if __name__ == '__main__':
     main_db = 'amazun.db'
 
-    #if os.path.exists(main_db):
-    #    os.remove(main_db)
-    #    print(f'{main_db} removed')
-#
+    if os.path.exists(main_db):
+        os.remove(main_db)
+        print(f'{main_db} removed')
+
     Base.metadata.create_all(bind=engine)
 
     store_manager.get_dummy_stores()
     books.get_dummy_books()
     books.get_dummy_authors()
 
+
     # Add 200 copies of Lord of The Rings to Store 1
     books.add_to_inventory(9780007117116, 1, 200, verbose=True)
 
-    # Add 4000 copies of ALL existing books to store 1:
-    books.add_all_books(store_id=1, copies=5000, verbose=True)
+    # Add 5000 copies of ALL existing books to store 2:
+    books.add_all_books(store_id=2, copies=5000, verbose=True)
 
-    # Add view
+    # Add view #TODO: Missing date of birth
     titles_by_author()
 
     # Add dummy customers
     customer.get_dummy_cst()
 
-    print('Complete')
+    # Search book
+    results = books.search_books("The")
+    for i in results:
+        print(i)
 
-    #TODO: cst purchases books (name variables updated to snake_case in models), check Customer_Books 
+    # Customer book purchase
+    customer.purchase_book(9780007117116, 1, 1, 10)
+    customer.purchase_book(9780007117116, 2, 2, 1)
+
+    # Move books
+    store_manager.move_books(9780007117116, 2, 3, 1000)
+
+
