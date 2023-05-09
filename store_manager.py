df146aac10add386aa6eb4db0d12dd9ea116afa3
William L <41084946+helveticae@users.noreply.github.com>
2023-05-09 14:43:51 +0200

    add move_books()

--- store_manager.py
+++ store_manager.py
@@ -31,6 +31,35 @@ def get_dummy_stores():
                 print(f"Store '{name}' already exists")
                 session.rollback()
 
+def move_books(isbn, from_store_id, to_store_id, quantity):
+    with Session() as session:
+        try:
+            # Check if there is enough stock at the source store
+            source_inventory = session.query(Inventory).filter_by(isbn13=isbn, StoreID=from_store_id).first()
+            if not source_inventory or source_inventory.stock < quantity:
+                print(f'Not enough ISBN {isbn} in stock at store {from_store_id} to fulfill transfer.')
+                return False
+
+            # Subtract stock from source store
+            source_inventory.stock -= quantity
+
+            # Add stock to destination store
+            destination_inventory = session.query(Inventory).filter_by(isbn13=isbn, StoreID=to_store_id).first()
+            if not destination_inventory:
+                destination_inventory = Inventory(isbn13=isbn, StoreID=to_store_id, stock=0)
+                session.add(destination_inventory)
+            destination_inventory.stock += quantity
+
+            session.commit()
+
+            print(f"{quantity} copies of ISBN {isbn} moved from store {from_store_id} to store {to_store_id}.")
+            return True
+
+        except IntegrityError as e:
+            print(f"Error: {e}. Rollback.")
+            session.rollback()
+            return False
+
 if __name__ == '__main__':
     Base.metadata.create_all(bind=engine) # Used to create and enforce constraints on Store
     add_store("Baloo2", "Sturegatan 13")
