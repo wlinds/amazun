048f01c6b6b15cf9be8a4ed813a865a192de5644
William L <41084946+helveticae@users.noreply.github.com>
2023-05-12 13:50:13 +0200

    Flask webapp

---
+++ app.py
@@ -0,0 +1,21 @@
+from models import *
+import os, store_manager, books, customer
+from Scripts.utils import titles_by_author, get_title, total_sales
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+database_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'amazun.db'))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_path}'
+db = SQLAlchemy(app)
+
+
+@app.route('/transactions')
+def view():
+    transaction_table = db.session.query(Transaction).all()
+    return render_template('transactions.html', results=transaction_table)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine)
+    app.run(debug=True)
