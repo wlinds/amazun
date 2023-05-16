b03df11b3d7e4e1a0d3b5c767366deeb947f3b2b
William L <41084946+helveticae@users.noreply.github.com>
2023-05-16 18:49:07 +0200

    add user authentication, login and authors page

--- app.py
+++ app.py
@@ -1,23 +1,73 @@
 from models import *
-import os, store_manager, books, customer
-from Scripts.utils import titles_by_author, get_title, total_sales, welcome_message
-from flask import Flask, render_template, request, redirect
+import os, secrets, store_manager, books, customer
+from Scripts.utils import titles_by_author, get_title, total_sales, welcome_message, drop_table
+from flask import Flask, render_template, request, redirect, session, current_app, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+
 database_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'amazun.db'))
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{database_path}'
 db = SQLAlchemy(app)
+
+# Define the User model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(256))
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)  # Update this line
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+def create_user(username, password):
+     #TODO: existing user exception handling (caused by unique constraint)
+    with current_app.app_context():
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user is not None and user.check_password(password):
+            flash('Successful authentication')
+            session['user_id'] = user.id
+            session['username'] = user.username
+            
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid username or password')
+
+    return render_template('login.html')
 
 @app.route('/base')
 def base():
     return render_template('base.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/dashboard')
+def dashboard():
+    # Access the user session
+    user_id = session.get('user_id')
+    username = session.get('username')
 
-    welcome = welcome_message(username='Shiva')
-    return render_template('index.html', message=welcome)
+    if user_id:
+        welcome = welcome_message(username)
+        return render_template('dashboard.html', greeting=welcome)
+    else:
+        # User is not authenticated, redirect
+        return redirect(url_for('login'))
 
 @app.route('/inventory', methods=['GET', 'POST'])
 def inventory():
@@ -45,7 +95,6 @@ def inventory():
                            results=inventory_table,
                            books=books_data,
                            stores=stores_data)
-
 
 @app.route('/transactions', methods=['GET', 'POST'])
 def transactions():
@@ -100,7 +149,6 @@ def customers():
     return render_template('customers.html',
                            results=customer_table)
 
-
 @app.route('/delete_customer', methods=['POST'])
 def delete_customer():
     customer_id = request.form.get('customer_id')
@@ -110,6 +158,28 @@ def delete_customer():
     # Redirect to after deletion
     return redirect('/customers')
 
+@app.route('/authors', methods=['GET', 'POST'])
+def authors():
+    author_table = db.session.query(Author).all()
+    return render_template('authors.html',
+                           results=author_table)
+
+@app.route('/delete_author', methods=['POST'])
+def delete_author():
+    author_id = request.form.get('author_id')
+    print(f"Author ID: {author_id}")
+    books.remove_author(author_id, verbose=True)
+
+    # Redirect to after deletion
+    return redirect('/authors')
+
 if __name__ == '__main__':
+
+    #drop_table('users')
+
+    with app.app_context():
+        db.create_all()
+        #create_user('admin', 'supersafe') #TODO: existing user exception handling (caused by unique constraint)
+
     Base.metadata.create_all(bind=engine)
     app.run(debug=True)
