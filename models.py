3bcde2ef7a9452fa5b21532d07f3e91b71aec459
William L <41084946+helveticae@users.noreply.github.com>
2023-05-29 20:02:20 +0200

    testing with MSSQL

--- models.py
+++ models.py
@@ -1,12 +1,19 @@
 from sqlalchemy import *
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 from datetime import datetime
+from my_credentials import cstring2
 
 # Define database connection
+# This has to be created first using Azure Data Studio or similar, maybe possible to do with sqlalchemy straight away, idk.
+engine = create_engine(cstring2)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
-engine = create_engine('sqlite:///amazun.db') # used to communicate with the database and execute SQL statements
-Session = sessionmaker(bind=engine)  # used to manage transactions and interact with the ORM layer of SQLAlchemy
-Base = declarative_base()  # model class represents a table in the database and its attributes represent columns
+# Use this for SQLite:
+
+#engine = create_engine('sqlite:///amazun.db') # used to communicate with the database and execute SQL statements
+#Session = sessionmaker(bind=engine)  # used to manage transactions and interact with the ORM layer of SQLAlchemy
+#Base = declarative_base()  # model class represents a table in the database and its attributes represent columns
 
 #TODO:
 # Improve constraints
@@ -17,10 +24,10 @@ Base = declarative_base()  # model class represents a table in the database and 
 class Author(Base):
     __tablename__ = 'Author'
     ID = Column(Integer, primary_key=True)
-    name = Column(String)
-    surname = Column(String)
-    birthdate = Column(String)
-    wiki = Column(String)
+    name = Column(String(255), nullable=False)
+    surname = Column(String(255), nullable=False)
+    birthdate = Column(String(255))
+    wiki = Column(String(255))
     books = relationship('Books', back_populates='author')
     books = relationship('Books', secondary='book_authors', back_populates='authors') # Association table
 
@@ -32,18 +39,18 @@ class Author(Base):
 
 # Association table
 book_authors = Table('book_authors', Base.metadata,
-    Column('book_id', Integer, ForeignKey('Books.isbn13')),
+    Column('book_id', String(13), ForeignKey('Books.isbn13')),
     Column('author_id', Integer, ForeignKey('Author.ID')),
 )
 
 class Books(Base):
     __tablename__ = 'Books'
-    isbn13 = Column(String, primary_key=True)
-    title = Column(String)
-    language = Column(String)
-    price = Column(Integer)
-    release = Column(String)
-    genre = Column(String, nullable=True)
+    isbn13 = Column(String(13), primary_key=True)
+    title = Column(String(255))
+    language = Column(String(255))
+    price = Column(Float)
+    release = Column(String(255))
+    genre = Column(String(255), nullable=True)
     AuthID = Column(Integer, ForeignKey('Author.ID'))
     author = relationship('Author', back_populates='books')
     authors = relationship('Author', secondary='book_authors', back_populates='books') # Association table
@@ -52,8 +59,8 @@ class Books(Base):
 class Store(Base):
     __tablename__ = 'Store'
     id = Column(Integer, primary_key=True)
-    store_name = Column(String, unique=True)
-    store_address = Column(String, unique=True)
+    store_name = Column(String(255), unique=True)
+    store_address = Column(String(255), unique=True)
     stock = relationship('Inventory', back_populates='store')
 
     __table_args__ = (
@@ -64,7 +71,7 @@ class Store(Base):
 class Inventory(Base):
     __tablename__ = 'Inventory'
     StoreID = Column(Integer, ForeignKey('Store.id'), primary_key=True)
-    isbn13 = Column(String, ForeignKey('Books.isbn13'), primary_key=True)
+    isbn13 = Column(String(13), ForeignKey('Books.isbn13'), primary_key=True)
     stock = Column(Integer)
     store = relationship('Store', back_populates='stock')
     book = relationship('Books', back_populates='inventory')
@@ -72,13 +79,13 @@ class Inventory(Base):
 class Customer(Base):
     __tablename__ = 'Customer'
     ID = Column(Integer, primary_key=True)
-    name = Column(String)
-    surname = Column(String)
-    address = Column(String)
-    city = Column(String)
-    state = Column(String)
-    zipcode = Column(String)
-    email = Column(String)
+    name = Column(String(255))
+    surname = Column(String(255))
+    address = Column(String(255))
+    city = Column(String(255))
+    state = Column(String(255))
+    zipcode = Column(String(255))
+    email = Column(String(255))
 
     created_at = Column(DateTime, default=datetime.now)
     deleted_at = Column(DateTime, nullable=True)
@@ -100,14 +107,14 @@ class Transaction(Base):
     StoreID = Column(Integer)
     book = relationship("Books")
     quantity = Column(Integer)
-    total_cost = Column(Numeric(precision=2))
+    total_cost = Column(Numeric(precision=10, scale=2))
 
 class ChangeLog(Base):
     __tablename__ = 'changelog'
 
     id = Column(Integer, primary_key=True)
-    table_name = Column(String)
-    action = Column(String)
+    table_name = Column(String(255))
+    action = Column(String(255))
     customer_id = Column(Integer)
     timestamp = Column(DateTime, default=datetime.now)
 
