147c6f9f68100a463c9415edc074c909d9148f61
William L <41084946+helveticae@users.noreply.github.com>
2023-08-14 17:50:23 +0200

    update names with nvarchar

--- store_manager.py
+++ store_manager.py
@@ -7,20 +7,37 @@ def search_books(search_term):
     """
 
     session = Session()
-    filter_book = (
-        session.query(Books, Inventory, Store)
+    filter_books = (
+        session.query(Book, Inventory, Store, Author)
         .join(Inventory.book)
         .join(Inventory.store)
-        .filter(Books.title.ilike(f'%{search_term}%'))
+        .join(Book.authors)
+        .filter(Book.title.ilike(f'%{search_term}%'))
         .all()
     )
+    
     results = {}
-    for book, inventory, store in filter_book:
-        if book.title in results:
-            results[book.title].append((store.store_name, inventory.stock))
+    for book, inventory, store, author in filter_books:
+        book_info = {
+            'title': book.title,
+            'isbn': book.isbn13,
+            'language': book.language,
+            'price': book.price,
+            'release_date': book.release_date,
+            'genre': book.genre,
+            'author': f"{author.first_name} {author.last_name}"
+        }
+        book_info_tuple = tuple(book_info.items())
+        if book_info_tuple in results:
+            results[book_info_tuple]['stores'].append((store.store_name, inventory.stock))
         else:
-            results[book.title] = [(store.store_name, inventory.stock)]
+            results[book_info_tuple] = {
+                'book_info': book_info,
+                'stores': [(store.store_name, inventory.stock)]
+            }
+    
     return results
+
 
 def add_store(name: str, address: str, verbose=False):
     """
@@ -38,10 +55,13 @@ def add_store(name: str, address: str, verbose=False):
             print(f"Store '{name}' already exists")
 
 def get_dummy_stores():
+    print('get_dummy_stores running')
     dummy_stores = [
         ('Toads Books', 'Mushroom Kingdom'),
         ('The Great Library', 'Alexandria'),
         ('Böcker & Babbel', 'Shinar'),
+        ('Stadsbiblioteket', 'Göteborg'),
+        ('中国出版集团', '中國'),
     ]
     with Session() as session:
         for name, address in dummy_stores:
@@ -57,7 +77,7 @@ def move_books(isbn, from_store_id, to_store_id, quantity):
     with Session() as session:
         try:
             # Check if there is enough stock at the source store
-            source_inventory = session.query(Inventory).filter_by(isbn13=isbn, StoreID=from_store_id).first()
+            source_inventory = session.query(Inventory).filter_by(isbn13=isbn, store_id=from_store_id).first()
             if not source_inventory or source_inventory.stock < quantity:
                 print(f'Not enough ISBN {isbn} in stock at store {from_store_id} to fulfill transfer.')
                 return False
@@ -66,9 +86,9 @@ def move_books(isbn, from_store_id, to_store_id, quantity):
             source_inventory.stock -= quantity
 
             # Add stock to destination store
-            destination_inventory = session.query(Inventory).filter_by(isbn13=isbn, StoreID=to_store_id).first()
+            destination_inventory = session.query(Inventory).filter_by(isbn13=isbn, store_id=to_store_id).first()
             if not destination_inventory:
-                destination_inventory = Inventory(isbn13=isbn, StoreID=to_store_id, stock=0)
+                destination_inventory = Inventory(isbn13=isbn, store_id=to_store_id, stock=0)
                 session.add(destination_inventory)
             destination_inventory.stock += quantity
 
@@ -88,7 +108,7 @@ def move_books(isbn, from_store_id, to_store_id, quantity):
 
 def add_all_books(store_id=1, copies=400, verbose=False):
     with Session() as session:
-        all_books = session.query(Books).all()
+        all_books = session.query(Book).all()
         for book in all_books:
             isbn = book.isbn13
             add_to_inventory(isbn, store_id, copies)
@@ -105,19 +125,20 @@ def add_to_inventory(isbn, store_id, copies, verbose=False):
     Add book to Inventory table.
     """
 
-    session = Session()
-    inventory = session.query(Inventory).filter_by(isbn13=isbn, StoreID=store_id).first()
-    if inventory:
-        inventory.stock += copies
-    else:
-        inventory = Inventory(isbn13=isbn, StoreID=store_id, stock=copies)
-        session.add(inventory)
-    session.commit()
+    with Session() as session:
+        inventory = session.query(Inventory).filter_by(isbn13=isbn, store_id=store_id).first()
+        if inventory:
+            inventory.stock += copies
+        else:
+            new_inventory = Inventory(isbn13=isbn, store_id=store_id, stock=copies)
+            session.add(new_inventory)
+        session.commit()
 
-    if verbose:
-        title = session.query(Books.title).filter_by(isbn13=isbn).one()[0]
-        store_name = session.query(Store.store_name).filter_by(id=store_id).one()[0]
-        print(f'Added {copies} copies of {title}, {isbn} to {store_name}.')
+        if verbose:
+            title = session.query(Book.title).filter_by(isbn13=isbn).scalar()
+            store_name = session.query(Store.store_name).filter_by(id=store_id).scalar()
+            print(f'Added {copies} copies of {title}, {isbn} to {store_name}.')
+
 
 
 if __name__ == '__main__':
